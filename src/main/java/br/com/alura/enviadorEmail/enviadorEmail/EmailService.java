0dc0275c26c87a3cc6eae9243dd63cc5b4209a7e
Jeanfrancisco <jeanfrancisco7@gmail.com>
2022-10-05 05:50:15 -0300

    Add projeto

--- src/main/java/br/com/alura/enviadorEmail/enviadorEmail/EmailService.java
+++ src/main/java/br/com/alura/enviadorEmail/enviadorEmail/EmailService.java
@@ -1,4 +1,29 @@
 package br.com.alura.enviadorEmail.enviadorEmail;
 
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailService {
+    public void enviar(String nome, String emailDestinatario){
+        try {
+            Email email = new SimpleEmail();
+            email.setHostName("smtp.gmail.com");
+            email.setSmtpPort(587);
+            email.setAuthenticator(new DefaultAuthenticator("email", "password"));
+            email.setSSLOnConnect(true);
+
+            email.setFrom("email");
+            email.setSubject("Você foi convidado pelo ListaVip");
+            email.setMsg("Olá "+ nome + ". Você acaba de ser convidado pelo ListaVip.");
+            email.addTo(emailDestinatario);
+            email.send();
+
+        }catch (EmailException e){
+            e.printStackTrace();
+        }
+    }
 }
